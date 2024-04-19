c7256630c374eab5eb08c6b24dc8b5cfb8a04f06
Matt Austen <mattausten@Matts-MacBook-Pro.local>
2024-04-18 22:09:02 -0400

    Prepare data code modularized

---
+++ FinalProject/config.py
@@ -0,0 +1,30 @@
+from neo4j import GraphDatabase
+import yaml
+
+# Quick function to print number of row/col of pandas dataframe
+def getNumRowsCols(df):
+    numRows = len(df.index)
+    numCols = len(df.columns)
+    return numRows,numCols
+
+def loadConfig():
+    with open('config.yaml', 'r') as file:
+        return yaml.safe_load(file)
+    
+config = loadConfig()
+
+def getNeo4jConnection():
+    return GraphDatabase.driver(
+        config['neo4j']['uri'],
+        auth=(config['neo4j']['user'], config['neo4j']['password']))
+
+def clearDatabase(driver):
+    with driver.session() as session:
+        session.run('MATCH (n) DETACH DELETE n')
+
+def quickCypher(neo4jDriver, query, verbose=False):
+    with neo4jDriver.session() as session:
+        result = session.run(query)
+        if verbose==True:
+            for record in result:
+                print(record)
---
+++ FinalProject/prepareData.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import numpy as np
+import config as cfg
+
+# Load Voteview.com data
+print(' ')
+print('Loading data...', end='')
+members = pd.read_csv('https://voteview.com/static/data/out/members/HSall_members.csv')  # Member Ideology
+parties = pd.read_csv('https://voteview.com/static/data/out/parties/HSall_parties.csv')  # Congressional Parties
+votes   = pd.read_csv('https://voteview.com/static/data/out/votes/HSall_votes.csv')      # Members' Votes
+print('Done.')
+
+
+# Add 'party_name', 'party_dim1_median', 'party_dim2_median', 'party_dim1_mean', 'party_dim2_mean' columns
+print('Preparing data...', end='')
+df = pd.merge(members, parties, on=['congress', 'party_code', 'chamber'], how='left')
+
+# Remove rows where chamber=='President'
+df = df[df['chamber'] != 'President']
+
+# Aggregate 'votes' on congress, chamber, icpsr... then add to df
+votes_tmp = votes.groupby(['congress', 'chamber', 'icpsr']).agg({'prob': 'mean', 'rollnumber': 'size'}).reset_index()
+df = pd.merge(df, votes_tmp, on=['congress', 'chamber', 'icpsr'], how='left')
+df = df.rename(columns={'prob': 'prob_nom', 'rollnumber': 'n_prob_nom'})
+df = df.fillna(value=np.nan)
+print('Done.')
+
+
+# Output to file
+print('Outputting data to csv...', end='')
+df.to_csv('HSall_custom.csv', index=False)
+
+# Output smaller version of file for testing purposes
+df_small = df[df['congress'].isin([1, 117, 118])]
+df_small.to_csv('HSall_custom_small.csv', index=False)
+print('Done.')
+
+# Print data sizes to screen
+print(' ')
+print('Member_Ideology:       ',cfg.getNumRowsCols(members))
+print('Congressional_Parties: ',cfg.getNumRowsCols(parties))
+print('Members_Votes:         ',cfg.getNumRowsCols(votes))
+print(' ')
+print('Final Dataframe:       ',cfg.getNumRowsCols(df))
+print(' ')
